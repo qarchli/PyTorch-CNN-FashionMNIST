f7c44fb76715f732539a4aa4718319ab0319d8e0
qarchli <qarchlimouad@gmail.com>
2020-03-23 23:32:43 +0100

    pass one run to train func instead of runs

--- main.py
+++ main.py
@@ -11,65 +11,65 @@ from torch.utils.tensorboard import SummaryWriter
 torch.set_printoptions(linewidth=120)
 
 
-def train(runs):
+def train(run):
     print('Getting the training data..')
     # downloading FashionMNIST if not already downloaded and putting it into data folder (E and T)
     train_set = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST', train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
 
     # start the training
     print('Starting the training process on {} examples..'.format(len(train_set)))
-    for run in runs:
-        print('\t', run)
-        # init the model: we can tune the model hyperparams by adding them to the run and passing them to the constructor
-        cnn = CNN()
-        # init the optimizer
-        optimizer = optim.Adam(cnn.parameters(), lr=run.lr)
-        # load the dataset
-        train_loader = torch.utils.data.DataLoader(train_set, batch_size=run.batch_size, shuffle=True)
-        # tensorboard session
-        images, labels = next(iter(train_loader))
-        grid = torchvision.utils.make_grid(images)
-        tb = SummaryWriter(comment=f'{run}')
-        tb.add_image('images', grid)
-        tb.add_graph(cnn, images)
 
-        for epoch in range(run.num_epochs):
-            total_loss = 0
-            total_correct = 0  # correctly predicted images
-            accuracy = 0
+    print('\t', run)
+    # init the model: we can tune the model hyperparams by adding them to the run and passing them to the constructor
+    cnn = CNN()
+    # init the optimizer
+    optimizer = optim.Adam(cnn.parameters(), lr=run.lr)
+    # load the dataset
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=run.batch_size, shuffle=True)
+    # tensorboard session
+    images, labels = next(iter(train_loader))
+    grid = torchvision.utils.make_grid(images)
+    tb = SummaryWriter(comment=f'{run}')
+    tb.add_image('images', grid)
+    tb.add_graph(cnn, images)
 
-            for batch in train_loader:
-                # get training batch from loader
-                images, labels = batch
+    for epoch in range(run.num_epochs):
+        total_loss = 0
+        total_correct = 0  # correctly predicted images
+        accuracy = 0
 
-                # pass the batch through the network
-                preds = cnn(images)
-                # compute the loss
-                loss = F.cross_entropy(preds, labels)
+        for batch in train_loader:
+            # get training batch from loader
+            images, labels = batch
 
-                # reinit the grads to avoid cumulativeness
-                optimizer.zero_grad()
-                # compute the gradients wrt weights
-                loss.backward()
-                # update the weights
-                optimizer.step()
+            # pass the batch through the network
+            preds = cnn(images)
+            # compute the loss
+            loss = F.cross_entropy(preds, labels)
 
-                total_loss += loss.item()
-                total_correct += F.softmax(preds, dim=1).argmax(dim=1).eq(labels).sum().item()
+            # reinit the grads to avoid cumulativeness
+            optimizer.zero_grad()
+            # compute the gradients wrt weights
+            loss.backward()
+            # update the weights
+            optimizer.step()
 
-            total_loss /= len(train_set)
-            accuracy = total_correct / len(train_set)
+            total_loss += loss.item()
+            total_correct += F.softmax(preds, dim=1).argmax(dim=1).eq(labels).sum().item()
 
-            tb.add_scalar('Loss', total_loss, epoch)
-            tb.add_scalar('Number correct', total_correct, epoch)
-            tb.add_scalar('accuracy', accuracy, epoch)
+        total_loss /= len(train_set)
+        accuracy = total_correct / len(train_set)
 
-            print('epoch: {}, total_correct: {}, loss: {}, accuracy: {}'.format(epoch, total_correct, total_loss, accuracy))
+        tb.add_scalar('Loss', total_loss, epoch)
+        tb.add_scalar('Number correct', total_correct, epoch)
+        tb.add_scalar('accuracy', accuracy, epoch)
 
-        tb.close()
-        print('Run ended..')
-        print('Saving model to disk..')
-        cnn.save(run)
+        print('epoch: {}, total_correct: {}, loss: {}, accuracy: {}'.format(epoch, total_correct, total_loss, accuracy))
+
+    tb.close()
+    print('Run ended..')
+    print('Saving model to disk..')
+    cnn.save(run)
     print('Training ended..')
 
 
@@ -105,10 +105,11 @@ def test(run):
 if __name__ == '__main__':
     # creating the hyperparams
     params = OrderedDict(lr=[0.01], batch_size=[100, 1000], num_epochs=[5])
+
     # building runs from hyperparams (cartesian product of all params)
     runs = RunBuilder.get_runs(params)
-    # training
-    train(runs)
-    # testing each run
+
+    # train and test each run
     for run in runs:
+        train(run)
         test(run)
